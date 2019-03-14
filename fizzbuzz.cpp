514957155c0138e64914abf8e2845657c3018b24
alexananev <48324900+alexananev@users.noreply.github.com>
2019-03-14 11:13:13 +0300

    Update fizzbuzz.cpp
    replacer has been brought in a separate .hpp file

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,35 +1,25 @@
+#include "fizzbuzz.hpp"					// header with replacer
+
 #include <iostream>
 #include <string>
 
+
 using namespace std;
 
-int main(){
-	string input;
-	while(cin >> input){
-		int div = 0;
-		int size = input.size();
-		for(int i = 0; i < size; i++){
-			switch (input[i]){
-				case '1': case '4': case '7': 
-						div += 1; break;
-				case '2': case '5': case '8':
-						div += 2; break;
-				
-			}
-			if (div > 2)
-				div -= 3;
-		}
-		if (div)
-			if (input[size-1] != '0' && input[size-1] != '5')
-				cout << input;
-			else cout << "buzz";
-		else{
-			cout << "fizz";
-			if (input[size-1] == '0' || input[size-1] == '5')
-				cout <<"buzz";
-		}
-		cout << " ";
-	}	
+int main(int argc, char* argv[]){
+	vector <string> input;
+	
+	for(int i = 1; i < argc; i++){
+		input.push_back(string(argv[i]));	
+	}
+	
+	vector <string> output = fizzbuzz(input); 	// calling replacer
+		
+	for(int i = 1; i < argc; i++){
+		cout << output[i] << " ";
+	}
+	cout << endl;
+	
 	
 	
 	
